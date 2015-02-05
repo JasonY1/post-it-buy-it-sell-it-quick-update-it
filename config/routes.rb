1283b5bc465c7272a5699ef4e810b16cb78ec4ee
JasonY1 <jasonh.yi@gmail.com>
2015-02-04 17:23:42 -0800

    Store: MVC

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
   root                    'static_pages#home'
   get 'about',        to: 'static_pages#about'
   get 'contact',      to: 'static_pages#contact'
-  get 'store',        to: 'store#index'
   get 'store/index'
 
 
