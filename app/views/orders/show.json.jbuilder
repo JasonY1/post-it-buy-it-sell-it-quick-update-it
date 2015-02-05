de458bbde98e760f83495ff56181f675b0def8e3
JasonY1 <jasonh.yi@gmail.com>
2015-02-04 18:00:17 -0800

    Order: MVC

--- app/views/orders/show.json.jbuilder
+++ app/views/orders/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @order, :id, :created_at, :updated_at
+json.extract! @order, :id, :express, :created_at, :updated_at
--- db/migrate/20150205015556_create_orders.rb
+++ db/migrate/20150205015556_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
+      t.boolean :express
 
       t.timestamps
     end
